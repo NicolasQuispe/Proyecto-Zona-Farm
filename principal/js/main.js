57bcd07dbd54843b2368c31f67b96498840045a6
imanolOliva <imanoleduardooliva@gmail.com>
2022-07-01 12:51:22 -0300

    Agregado Preguntas frecuentes

--- principal/js/main.js
+++ principal/js/main.js
@@ -1,1 +1,15 @@
-alert('perro')
+var preguntas = document.querySelectorAll('.preguntas .contenedor-pregunta')
+preguntas.forEach((pregunta) =>{
+    pregunta.addEventListener('click' , (e)=>{
+       
+        e.currentTarget.classList.toggle('activa'); 
+
+       var respuesta = pregunta.querySelector('.respuesta')
+       var alturaReal = respuesta.scrollHeight;
+       if(!respuesta.style.maxHeight){
+          respuesta.style.maxHeight = alturaReal + 'px';
+       }else{
+        respuesta.style.maxHeight = null
+       }
+    })
+}); 
